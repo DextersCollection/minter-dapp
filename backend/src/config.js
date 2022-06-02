c29c0aab14c6cbe554e72b7d892c6ab49d9d8e37
DextersColletion <cody.w.turner817@gmail.com>
2022-06-02 18:33:20 -0400

    updated link

--- backend/src/config.js
+++ backend/src/config.js
@@ -86,7 +86,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x345EAA7d85684084C2ce454ECe3cb3Ce91c7776B"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x345EAA7d85684084C2ce454ECe3cb3Ce91c7776B"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "0xbd4Dd873fDd0612A74aDEbDa37c8C84eEB6354E5"; // If you want to manually include it
